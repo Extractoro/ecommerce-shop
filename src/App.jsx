e86ba5b7b86ffb463a411828f6996bea1bea8e0c
Vadym <67538058+Extractoro@users.noreply.github.com>
2024-01-21 11:52:55 +0200

    start: init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
